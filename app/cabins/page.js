cd05af7b949d1a5b4f9870816c7282e47fdc539d
Kazem Kamali <itskazemk@gmail.com>
2024-07-06 09:50:43 +0330

    016 Displaying a Loading Indicator.mp4

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,4 +1,21 @@
-export default function Cabins() {
-    return <h3>Cabins</h3>;
-  }
-  
+import Counter from "../components/Counter";
+
+export default async function Cabins() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users").then(
+    (res) => res.json()
+  );
+  console.log(data);
+
+  return (
+    <div>
+      <h3>Cabins</h3>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+      <Counter />
+    </div>
+  );
+}
